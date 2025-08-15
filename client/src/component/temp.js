8abe67ab8120db666c86b3a1f80ebf5daed351fa
adarshXpal <chandra7piyush@gmail.com>
2025-08-15 18:19:40 +0530

    sidebar done

---
+++ client/src/component/temp.js
@@ -0,0 +1,57 @@
+export const products = [
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+    {
+        name: "piyush",
+        image: "https://yt3.ggpht.com/yti/ANjgQV8FcipRrroXPP6So9rSr4N7XG53ORkKjCBiOHHRMEqQc2Y=s108-c-k-c0x00ffffff-no-rj",
+        message: "Hi! How are you",
+    },
+];
