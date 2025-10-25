f7dfb5f05d4be1401ca45d414e82559da186f3c6
piyush <21bcs107@nith.ac.in>
2025-10-25 18:57:52 +0530

    handled resize

--- frontend/src/components/Home/Layout.js
+++ frontend/src/components/Home/Layout.js
@@ -12,9 +12,12 @@ function Layout() {
     const { currentUser } = useUsers();
     const [isMobile, setIsMobile] = useState(false);
     useEffect(() => {
-        window.addEventListener("resize", () =>
-            setIsMobile(window.innerWidth <= 768)
-        );
+        const handleResize = () => setIsMobile(window.innerWidth <= 768);
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
     }, []);
 
     if (loading) {
