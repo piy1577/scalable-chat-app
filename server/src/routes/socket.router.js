40273d3c39d3eae9f43dda8ff7089c0053d472d4
piyush <21bcs107@nith.ac.in>
2025-11-30 03:17:39 +0530

    update sockets also

--- server/src/routes/socket.router.js
+++ server/src/routes/socket.router.js
@@ -2,13 +2,8 @@ const socketIo = require("socket.io");
 const {
     socketConnect,
     sendMessage,
-    seenMessage,
     disconnectSocket,
-    startTyping,
-    stopTyping,
-    updateUsers,
-    addRoom,
-} = require("../controller/socket.controller");
+} = require("../controller/mms/sendMessage.controller");
 
 let io;
 
@@ -22,18 +17,8 @@ const connectSocket = (server) => {
 
     io.on("connection", (socket) => {
         socket.on("register", (userId) => socketConnect(socket, userId));
-
         socket.on("send_message", (data) => sendMessage(socket, data));
-        socket.on("seen_message", (data) => seenMessage(socket, data));
-
-        socket.on("start_typing", (data) => startTyping(socket, data));
-        socket.on("stop_typing", (data) => stopTyping(socket, data));
-
         socket.on("disconnect", () => disconnectSocket(socket));
-
-        socket.on("update_users", (data) => updateUsers(io, socket, data));
-
-        socket.on("add_room", (data) => addRoom(io, socket, data));
 
         socket.on("error", (error) => {
             console.error("Socket error:", error);
---
+++ server/src/services/user-mapping.service.js
@@ -0,0 +1,54 @@
+class UserMappingService {
+    static users = new Map();
+    static rooms = new Map();
+    static sockets = new Map();
+    static socketIds = new Map();
+
+    static addUserToRooms = (userId, roomIds) => {
+        this.users.set(userId, roomIds);
+
+        for (const roomId of roomIds) {
+            const roomUsers = this.rooms.get(roomId) || [];
+
+            if (!roomUsers.includes(userId)) {
+                this.rooms.set(roomId, [...roomUsers, userId]);
+            }
+        }
+    };
+
+    static getRoomUsers = (roomId) => {
+        const userIds = this.rooms.get(roomId);
+        if (!userIds) return [];
+
+        const socketIds = userIds.map((t) => socketIds.get(t));
+        return socketIds;
+    };
+
+    static register = (userId, socket) => {
+        this.socketIds.set(socket?.id, userId);
+        this.sockets.set(socket);
+    };
+
+    static disconnect = (socketId) => {
+        const userId = this.socketIds.get(socketId);
+        this.socketIds.delete(socketId);
+        this.sockets.delete(userId);
+
+        const rooms = this.users.get(userId) || [];
+
+        for (const roomId of rooms) {
+            const roomUsers = this.rooms.get(roomId);
+            if (!roomUsers) continue;
+            const updatedRoomUsers = roomUsers.filter((u) => u !== userId);
+            if (updatedRoomUsers.length === 0) {
+                this.rooms.delete(roomId);
+            } else {
+                this.rooms.set(roomId, updatedRoomUsers);
+            }
+        }
+
+        this.users.delete(userId);
+    };
+}
+
+module.exports = UserMappingService;
